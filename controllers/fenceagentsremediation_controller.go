e7c968aad935f7b378d6cab8494f5c835f87c6eb
Michael Shitrit <mshitrit@redhat.com>
2025-04-16 12:03:26 +0300

    a more descriptive node secret name
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- controllers/fenceagentsremediation_controller.go
+++ controllers/fenceagentsremediation_controller.go
@@ -337,11 +337,12 @@ func (r *FenceAgentsRemediationReconciler) updateStatus(ctx context.Context, far
 
 func (r *FenceAgentsRemediationReconciler) collectSecretParams(far *v1alpha1.FenceAgentsRemediation, ctx context.Context) (map[string]string, error) {
 	secretParams := map[string]string{}
-	nodeSecretName := far.Name
+	nodeName := far.Name
 	// fetch secret/node name from remediation's annotation if present
 	if annotatedName, exist := far.Annotations["remediation.medik8s.io/node-name"]; exist {
-		nodeSecretName = annotatedName
+		nodeName = annotatedName
 	}
+	nodeSecretName := fmt.Sprintf("fence-agents-credentials-node-%s", nodeName)
 	nodeSecret, err := r.getParamSecret(ctx, client.ObjectKey{Name: nodeSecretName, Namespace: far.Namespace})
 	if err != nil && !apiErrors.IsNotFound(err) {
 		r.Log.Error(err, "failed to fetch secret", "secret name", nodeSecretName, "namespace", far.Namespace)
