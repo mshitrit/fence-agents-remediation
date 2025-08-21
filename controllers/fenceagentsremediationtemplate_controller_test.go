a0b48564007fde28e2ef2e29ca792b00ef9476b8
Michael Shitrit <mshitrit@redhat.com>
2025-08-21 16:24:11 +0300

    refactor and add unit tests
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- controllers/fenceagentsremediationtemplate_controller_test.go
+++ controllers/fenceagentsremediationtemplate_controller_test.go
@@ -22,6 +22,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/medik8s/fence-agents-remediation/api/v1alpha1"
@@ -29,89 +30,136 @@ import (
 )
 
 var _ = Describe("FART Controller", func() {
-	It("sets ParametersValidation=True and no validation failures on happy flow", func() {
-		fart := &v1alpha1.FenceAgentsRemediationTemplate{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "tmpl-happy",
-				Namespace: defaultNamespace,
-			},
-			Spec: v1alpha1.FenceAgentsRemediationTemplateSpec{
-				Template: v1alpha1.FenceAgentsRemediationTemplateResource{
-					Spec: v1alpha1.FenceAgentsRemediationSpec{
-						Agent: "fence_ipmilan",
-						SharedParameters: map[v1alpha1.ParameterName]string{
-							"--username": "admin",
-							"--password": "password",
-						},
-						NodeParameters: map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string{
-							"--ip": {v1alpha1.NodeName("worker-1"): cli.SuccessfulStatusCheckIp},
-						},
+	var fart *v1alpha1.FenceAgentsRemediationTemplate
+
+	Context("Fence Status Validation", func() {
+		JustBeforeEach(func() {
+			Expect(k8sClient.Create(context.Background(), fart)).To(Succeed())
+			DeferCleanup(func() { _ = k8sClient.Delete(context.Background(), fart) })
+		})
+		When("Single node status is ok", func() {
+			BeforeEach(func() {
+				fart = &v1alpha1.FenceAgentsRemediationTemplate{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "tmpl-happy",
+						Namespace: defaultNamespace,
 					},
-				},
-			},
-		}
-
-		Expect(k8sClient.Create(context.Background(), fart)).To(Succeed())
-		DeferCleanup(func() { _ = k8sClient.Delete(context.Background(), fart) })
-
-		Eventually(func(g Gomega) {
-			updated := &v1alpha1.FenceAgentsRemediationTemplate{}
-			g.Expect(k8sClient.Get(context.Background(), client.ObjectKeyFromObject(fart), updated)).To(Succeed())
-
-			g.Expect(updated.Status.ValidationFailures).To(BeEmpty())
-
-			cond := meta.FindStatusCondition(updated.Status.Conditions, ConditionParametersValidation)
-			g.Expect(cond).NotTo(BeNil())
-			g.Expect(cond.Status).To(Equal(metav1.ConditionTrue))
-			g.Expect(cond.Reason).To(Equal(ReasonValidationSucceeded))
-		}, "5s", "200ms").Should(Succeed())
-	})
-
-	It("handles 3 nodes: success, timeout, non-ON status", func() {
-		fart := &v1alpha1.FenceAgentsRemediationTemplate{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "tmpl-mixed",
-				Namespace: defaultNamespace,
-			},
-			Spec: v1alpha1.FenceAgentsRemediationTemplateSpec{
-				Template: v1alpha1.FenceAgentsRemediationTemplateResource{
-					Spec: v1alpha1.FenceAgentsRemediationSpec{
-						Agent: "fence_ipmilan",
-						SharedParameters: map[v1alpha1.ParameterName]string{
-							"--username": "admin",
-							"--password": "password",
-						},
-						NodeParameters: map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string{
-							"--ip": {
-								v1alpha1.NodeName("worker-1"): cli.SuccessfulStatusCheckIp, // success
-								v1alpha1.NodeName("worker-2"): cli.TimedOutStatusCheckIp,   // timeout
-								v1alpha1.NodeName("worker-3"): cli.OffStatusCheckIp,        // not-ON
+					Spec: v1alpha1.FenceAgentsRemediationTemplateSpec{
+						Template: v1alpha1.FenceAgentsRemediationTemplateResource{
+							Spec: v1alpha1.FenceAgentsRemediationSpec{
+								Agent: "fence_ipmilan",
+								SharedParameters: map[v1alpha1.ParameterName]string{
+									"--username": "admin",
+									"--password": "password",
+								},
+								NodeParameters: map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string{
+									"--ip": {"worker-1": cli.SuccessfulStatusCheckIp},
+								},
 							},
 						},
 					},
-				},
-			},
-		}
+				}
+			})
+			It("Status validation should succeed", func() {
+				verifySuccess(fart)
+			})
+		})
+		When("2/3  nodes status isn't ok", func() {
+			BeforeEach(func() {
+				fart = &v1alpha1.FenceAgentsRemediationTemplate{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "tmpl-mixed",
+						Namespace: defaultNamespace,
+					},
+					Spec: v1alpha1.FenceAgentsRemediationTemplateSpec{
+						Template: v1alpha1.FenceAgentsRemediationTemplateResource{
+							Spec: v1alpha1.FenceAgentsRemediationSpec{
+								Agent: "fence_ipmilan",
+								SharedParameters: map[v1alpha1.ParameterName]string{
+									"--username": "admin",
+									"--password": "password",
+								},
+								NodeParameters: map[v1alpha1.ParameterName]map[v1alpha1.NodeName]string{
+									"--ip": {
+										"worker-1": cli.SuccessfulStatusCheckIp, // success
+										"worker-2": cli.TimedOutStatusCheckIp,   // timeout
+										"worker-3": cli.OffStatusCheckIp,        // not-ON
+									},
+								},
+							},
+						},
+					},
+				}
+			})
+			It("failed status should report correctly", func() {
+				Eventually(func(g Gomega) {
+					updated := &v1alpha1.FenceAgentsRemediationTemplate{}
+					g.Expect(k8sClient.Get(context.Background(), client.ObjectKeyFromObject(fart), updated)).To(Succeed())
 
-		Expect(k8sClient.Create(context.Background(), fart)).To(Succeed())
-		DeferCleanup(func() { _ = k8sClient.Delete(context.Background(), fart) })
+					cond := meta.FindStatusCondition(updated.Status.Conditions, ConditionParametersValidation)
+					g.Expect(cond).NotTo(BeNil())
+					g.Expect(cond.Status).To(Equal(metav1.ConditionFalse))
+					g.Expect(cond.Reason).To(Equal(ReasonValidationFailed))
 
-		Eventually(func(g Gomega) {
-			updated := &v1alpha1.FenceAgentsRemediationTemplate{}
-			g.Expect(k8sClient.Get(context.Background(), client.ObjectKeyFromObject(fart), updated)).To(Succeed())
+					g.Expect(updated.Status.ValidationFailures).To(HaveLen(2))
+					// worker-2 timed out
+					g.Expect(updated.Status.ValidationFailures).To(HaveKey("worker-2"))
+					g.Expect(updated.Status.ValidationFailures["worker-2"]).To(ContainSubstring("timed out"))
+					// worker-3 not ON
+					g.Expect(updated.Status.ValidationFailures).To(HaveKey("worker-3"))
+					g.Expect(updated.Status.ValidationFailures["worker-3"]).To(ContainSubstring("not ON"))
+				}, "12s", "200ms").Should(Succeed())
+			})
+			Context("StatusValidationSample configuration ", func() {
+				When("StatusValidationSample is set to check one node", func() {
+					BeforeEach(func() {
+						fart.Spec.Template.Spec.StatusValidationSample = &intstr.IntOrString{Type: intstr.Int, IntVal: 1}
+					})
+					It("Status validation should succeed", func() {
+						verifySuccess(fart)
+					})
+				})
+				When("StatusValidationSample is set to check 66% of the nodes", func() {
+					BeforeEach(func() {
+						fart.Spec.Template.Spec.StatusValidationSample = &intstr.IntOrString{Type: intstr.String, StrVal: "66%"}
+					})
+					It("Status validation should fails for node 2 and omit for node 3", func() {
+						Eventually(func(g Gomega) {
+							updated := &v1alpha1.FenceAgentsRemediationTemplate{}
+							g.Expect(k8sClient.Get(context.Background(), client.ObjectKeyFromObject(fart), updated)).To(Succeed())
 
-			cond := meta.FindStatusCondition(updated.Status.Conditions, ConditionParametersValidation)
-			g.Expect(cond).NotTo(BeNil())
-			g.Expect(cond.Status).To(Equal(metav1.ConditionFalse))
-			g.Expect(cond.Reason).To(Equal(ReasonValidationFailed))
+							cond := meta.FindStatusCondition(updated.Status.Conditions, ConditionParametersValidation)
+							g.Expect(cond).NotTo(BeNil())
+							g.Expect(cond.Status).To(Equal(metav1.ConditionFalse))
+							g.Expect(cond.Reason).To(Equal(ReasonValidationFailed))
 
-			g.Expect(updated.Status.ValidationFailures).To(HaveLen(2))
-			// worker-2 timed out
-			g.Expect(updated.Status.ValidationFailures).To(HaveKey("worker-2"))
-			g.Expect(updated.Status.ValidationFailures["worker-2"]).To(ContainSubstring("timed out"))
-			// worker-3 not ON
-			g.Expect(updated.Status.ValidationFailures).To(HaveKey("worker-3"))
-			g.Expect(updated.Status.ValidationFailures["worker-3"]).To(ContainSubstring("not ON"))
-		}, "12s", "200ms").Should(Succeed())
+							g.Expect(updated.Status.ValidationFailures).To(HaveLen(1))
+							// worker-2 timed out
+							g.Expect(updated.Status.ValidationFailures).To(HaveKey("worker-2"))
+							g.Expect(updated.Status.ValidationFailures["worker-2"]).To(ContainSubstring("timed out"))
+							// worker-3 not ON
+						}, "12s", "200ms").Should(Succeed())
+					})
+				})
+			})
+		})
 	})
+
 })
+
+func verifySuccess(fart *v1alpha1.FenceAgentsRemediationTemplate) {
+
+	Eventually(func(g Gomega) {
+		updated := &v1alpha1.FenceAgentsRemediationTemplate{}
+		g.Expect(k8sClient.Get(context.Background(), client.ObjectKeyFromObject(fart), updated)).To(Succeed())
+
+		g.Expect(updated.Status.ValidationFailures).To(BeEmpty())
+
+		cond := meta.FindStatusCondition(updated.Status.Conditions, ConditionParametersValidation)
+		g.Expect(cond).NotTo(BeNil())
+		g.Expect(cond.Status).To(Equal(metav1.ConditionTrue))
+		g.Expect(cond.Reason).To(Equal(ReasonValidationSucceeded))
+	}, "5s", "200ms").Should(Succeed())
+}
+
+//TODO mshitrit implement disabled configuration
