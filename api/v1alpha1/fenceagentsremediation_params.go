0c04d67451dfa84cc34c479e8a127e347b3905ca
Michael Shitrit <mshitrit@redhat.com>
2025-08-25 14:49:47 +0300

    remove 13 sec timeout extension
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- api/v1alpha1/fenceagentsremediation_params.go
+++ api/v1alpha1/fenceagentsremediation_params.go
@@ -20,8 +20,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	commonAnnotations "github.com/medik8s/common/pkg/annotations"
 	"maps"
+
+	commonAnnotations "github.com/medik8s/common/pkg/annotations"
 
 	corev1 "k8s.io/api/core/v1"
 	apiErrors "k8s.io/apimachinery/pkg/api/errors"
@@ -50,8 +51,7 @@ var (
 	paramsLog = logf.Log.WithName("fenceagentsremediation-params")
 )
 
-// Extending the default 10 sec timeout to 13 per ocp cap because we are running multiple status validation and want to take advantage of the maximum possible time (https://docs.redhat.com/en/documentation/openshift_container_platform/4.19/html/architecture/admission-plug-ins)
-// +kubebuilder:webhook:path=/validate-fence-agents-remediation-medik8s-io-v1alpha1-fenceagentsremediationtemplate,mutating=false,failurePolicy=fail,sideEffects=None,timeoutSeconds=13,groups=fence-agents-remediation.medik8s.io,resources=fenceagentsremediationtemplates,verbs=create;update,versions=v1alpha1,name=vfenceagentsremediationtemplate.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-fence-agents-remediation-medik8s-io-v1alpha1-fenceagentsremediationtemplate,mutating=false,failurePolicy=fail,sideEffects=None,groups=fence-agents-remediation.medik8s.io,resources=fenceagentsremediationtemplates,verbs=create;update,versions=v1alpha1,name=vfenceagentsremediationtemplate.kb.io,admissionReviewVersions=v1
 
 type customValidator struct {
 	client.Client
