d18e773952f599f6663fc1d6a7f9b416f50d1d86
Michael Shitrit <mshitrit@redhat.com>
2026-01-12 11:18:21 +0200

    Align description to cond
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- api/v1alpha1/fenceagentsremediationtemplate_types.go
+++ api/v1alpha1/fenceagentsremediationtemplate_types.go
@@ -40,7 +40,7 @@ type FenceAgentsRemediationTemplateSpec struct {
 // FenceAgentsRemediationTemplateStatus defines the observed state of FenceAgentsRemediationTemplate
 type FenceAgentsRemediationTemplateStatus struct {
 	// Represents the observations of a FenceAgentsRemediationTemplate's current state.
-	// Known .status.conditions.type: "ParametersValidated".
+	// Known .status.conditions.type: "ParametersValidation".
 	// +listType=map
 	// +listMapKey=type
 	// +optional
